cb18a25005db609e5a38828366f60783ff4ed85c
OYYM <1052016884@qq.com>
2017-07-10 21:21:14 +0800

    修改

--- frontend/controllers/FileTokenController.php
+++ frontend/controllers/FileTokenController.php
@@ -9,20 +9,37 @@ use Qiniu\Auth;
 
 class FileTokenController extends WebController
 {
-
+    /**
+     * @SWG\Get(path="/file-token/index?debug=1",
+     *   tags={"获取七牛云token"},
+     *   summary="获取七牛云token",
+     *   description="Author: OYYM",
+     *   @SWG\Parameter(
+     *     name="token_type",
+     *     in="query",
+     *     default="1",
+     *     description="不传参数默认给image的token, 1 = video公有，2 = video私有桶 , 3 = image私有",
+     *     required=true,
+     *     type="integer",
+     *   ),
+     *   @SWG\Response(
+     *       response=200,description="successful operation"
+     *   )
+     * )
+     */
     public function actionIndex()
     {
         require_once '../../vendor/qiniu/php-sdk/autoload.php';
         $auth = new Auth(Yii::$app->params['qiniu_access_key'], Yii::$app->params['qiniu_secret_key']);
-        if (Yii::$app->request->get('video')) {
+        if (Yii::$app->request->get('token_type') == 1) {
             $bkt = Yii::$app->params['qiniu_bucket_videos'];
             $upToken = $auth->uploadToken($bkt);
             $data = array('uptoken' => $upToken, 'host' => Yii::$app->params['qiniu_url_images']);
-        } elseif (Yii::$app->request->get('video-private')) {
+        } elseif (Yii::$app->request->get('token_type') == 2) {
             $bkt = Yii::$app->params['qiniu_bucket_videos_private'];
             $upToken = $auth->uploadToken($bkt);
             $data = array('uptoken' => $upToken, 'host' => Yii::$app->params['qiniu_url_images']);
-        } elseif (Yii::$app->request->get('image-private')) {
+        } elseif (Yii::$app->request->get('token_type' == 3)) {
             $bkt = Yii::$app->params['qiniu_bucket_images_private'];
             $upToken = $auth->uploadToken($bkt);
             $data = array('uptoken' => $upToken, 'host' => Yii::$app->params['qiniu_url_images']);
